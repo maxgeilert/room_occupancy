71a9ee13f16e2cc02f9ee55b3af8a231797551d5
Max Geilert <mx.g@live.de>
2022-07-13 16:52:46 +0200

    Update const.py

--- custom_components/room_occupancy/const.py
+++ custom_components/room_occupancy/const.py
@@ -4,4 +4,13 @@ TIMEOUT = 2
 ROOMNAME = "Livingroom"
 ENTITIES_TOGGLE = ["binary_sensor.livingroom_motion"]
 ENTITIES_KEEP = ["binary_sensor.livingroom_occupancy"]
-ACTIVE_STATES = ["active", "on", True, "occupied"]
+DEFAULT_ACTIVE_STATES = ["active", "on", True, "occupied", 1]
+
+DEFAULT_NAME = "Room Occupancy Sensor"
+CONF_ROOMNAME = "roomname"
+DEFAULT_ROOMNAME = "exampleroom"
+CONF_TIMEOUT = "timeout"
+DEFAULT_TIMEOUT = 2
+CONF_ENTITIES_TOGGLE = "entities_toggle"
+CONF_ENTITIES_KEEP = "entities_keep"
+CONF_ACTIVE_STATES = "active_states"
