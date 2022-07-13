fb4751beb934f7b83de24be698f774d6e10f6c81
Max Geilert <mx.g@live.de>
2022-07-13 16:51:53 +0200

    Update binary_sensor.py

--- custom_components/room_occupancy/binary_sensor.py
+++ custom_components/room_occupancy/binary_sensor.py
@@ -1,8 +1,7 @@
-"""Use Bayesian Inference to trigger a binary sensor."""
+"""Use multiple sensors to control a binary sensor."""
 from __future__ import annotations
 
 import logging
-from sqlalchemy import false
 
 import voluptuous as vol
 
@@ -10,14 +9,10 @@ import homeassistant.helpers.event as eventHelper
 from homeassistant.components.binary_sensor import (
     BinarySensorEntity,
     PLATFORM_SCHEMA,
-    DOMAIN,
     STATE_OFF,
-    STATE_ON,
     DEVICE_CLASS_OCCUPANCY,
 )
 from homeassistant.const import (
-    CONF_ABOVE,
-    CONF_BELOW,
     CONF_DEVICE_CLASS,
     CONF_ENTITY_ID,
     CONF_NAME,
@@ -25,7 +20,6 @@ from homeassistant.const import (
     CONF_STATE,
     CONF_VALUE_TEMPLATE,
     STATE_OFF,
-    STATE_UNKNOWN,
 )
 from homeassistant.core import HomeAssistant, State, callback
 from homeassistant.exceptions import ConditionError, TemplateError
@@ -36,25 +30,18 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.template import result_as_boolean
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from .const import DOMAIN, PLATFORMS
+from .const import *
 
 _LOGGER = logging.getLogger(__name__)
 
-DEFAULT_NAME = "Room Occupancy Sensor"
-CONF_ROOMNAME = "roomname"
-DEFAULT_ROOMNAME = "exampleroom"
-CONF_TIMEOUT = "timeout"
-DEFAULT_TIMEOUT = 2
-CONF_ENTITIES_TOGGLE = "entities_toggle"
-CONF_ENTITITES_KEEP = "entities_keep"
-CONF_ACTIVE_STATES = "active_states"
+
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     {
         vol.Required(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Required(CONF_ROOMNAME, default=DEFAULT_ROOMNAME): cv.string,
         vol.Optional(CONF_TIMEOUT, default=DEFAULT_TIMEOUT): cv.positive_int,
         vol.Required(CONF_ENTITIES_TOGGLE, default=[]): cv.ensure_list,
-        vol.Required(CONF_ENTITITES_KEEP, default=[]): cv.ensure_list,
+        vol.Required(CONF_ENTITIES_KEEP, default=[]): cv.ensure_list,
         vol.Optional(
             CONF_ACTIVE_STATES, default=["on", "occupied", 1, True, "active"]
         ): cv.ensure_list,
@@ -68,7 +55,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     roomname = config.get(CONF_ROOMNAME)
     timeout = config.get(CONF_TIMEOUT)
     entities_toggle = config.get(CONF_ENTITIES_TOGGLE)
-    entities_keep = config.get(CONF_ENTITITES_KEEP)
+    entities_keep = config.get(CONF_ENTITIES_KEEP)
     active_states = config.get(CONF_ACTIVE_STATES)
 
     _LOGGER.debug("setup_platform triggered!")
@@ -98,7 +85,7 @@ class RoomOccupancyBinarySensor(BinarySensorEntity):
             CONF_ROOMNAME: config.get(CONF_ROOMNAME),
             CONF_TIMEOUT: config.get(CONF_TIMEOUT),
             CONF_ENTITIES_TOGGLE: config.get(CONF_ENTITIES_TOGGLE),
-            CONF_ENTITITES_KEEP: config.get(CONF_ENTITITES_KEEP),
+            CONF_ENTITIES_KEEP: config.get(CONF_ENTITIES_KEEP),
             CONF_ACTIVE_STATES: config.get(CONF_ACTIVE_STATES),
         }
         self._state = STATE_OFF
@@ -109,7 +96,7 @@ class RoomOccupancyBinarySensor(BinarySensorEntity):
             self._name,
             self.attr[CONF_ROOMNAME],
             self.attr[CONF_ENTITIES_TOGGLE],
-            self.attr[CONF_ENTITITES_KEEP],
+            self.attr[CONF_ENTITIES_KEEP],
             self.attr[CONF_TIMEOUT],
             self._state,
             self.attr[CONF_ACTIVE_STATES],
@@ -117,7 +104,7 @@ class RoomOccupancyBinarySensor(BinarySensorEntity):
 
         eventHelper.track_state_change(
             self.hass,
-            self.attr[CONF_ENTITIES_TOGGLE] + self.attr[CONF_ENTITITES_KEEP],
+            self.attr[CONF_ENTITIES_TOGGLE] + self.attr[CONF_ENTITIES_KEEP],
             self.entity_state_changed,
         )
 
@@ -140,7 +127,7 @@ class RoomOccupancyBinarySensor(BinarySensorEntity):
 
         if self._state is "occupied":
             use_entities = (
-                self.attr[CONF_ENTITIES_TOGGLE] + self.attr[CONF_ENTITITES_KEEP]
+                self.attr[CONF_ENTITIES_TOGGLE] + self.attr[CONF_ENTITIES_KEEP]
             )
         else:
             use_entities = self.attr[CONF_ENTITIES_TOGGLE]
@@ -160,7 +147,7 @@ class RoomOccupancyBinarySensor(BinarySensorEntity):
                 _LOGGER.debug("entity is inactive!")
         if found:
             self._state = "occupied"
-            #self.hass.state.set()
+            # self.hass.state.set()
         else:
             self._state = "not occupied"
         _LOGGER.debug("finished setting state, _state is: %s" % self._state)
